c4038a53d882b86bc1a8798c712a2d4b7df9347e
456rtyfgh <96326355+456rtyfgh@users.noreply.github.com>
2022-02-17 15:55:40 +0900

    Create main1.py

---
+++ 8강/main1.py
@@ -0,0 +1,40 @@
+# number = 0
+# while number != 100:
+#     print("100입력시 시작 끝내고 싶을땐 컨트롤 씨를 누르시오.")
+#     number = int(input())
+
+
+
+#  n = 0
+#  while True:
+#      n = n + 1
+#      print(n)
+
+# number = 0
+# max = int(input())
+
+# while number < max:
+#     number = number + 1
+#     print (number)
+    
+# for number in range(1,366,1):
+#     print("{}일".format(number))
+    
+# 단 = int(input('구구단을 출력합니다. 몇 단인지 입력해 주세요
+# for number in range(10):
+#     변수1= '{}x{}={}'.format(단, number, 단 * number)
+#     print (변수1)
+    
+# for number in range(4):
+#     if number is 2:
+#         continue
+#     print(number)
+
+
+# for number in range(4):
+#     if number is 2:
+#         break
+#     print(number)
+    
+
+
