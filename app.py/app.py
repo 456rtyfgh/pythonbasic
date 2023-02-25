14198808080796ba93e65a7580667417f1fecc6f
456rtyfgh <96326355+456rtyfgh@users.noreply.github.com>
2023-02-25 17:11:39 +0900

    Create app.py

---
+++ app.py/app.py
@@ -0,0 +1,69 @@
+#1번
+
+#print('\'python\'은 프로그래밍 언어입니다.')
+
+#2번
+
+# #화
+# temperature1 = 32
+
+# #섭
+# temperature2 = 32
+
+# convert1 = (temperature1 - 32) % 1.8
+# convert2 = (temperature2 * 1.8) + 32
+
+# print ('화씨 {}도는 섭씨 {}도.'.format(temperature1,convert1))
+# print ('섭씨 {}도는 화씨 {}도.'.format(temperature2,convert2))
+
+#3번
+
+#year = 2023
+
+#4번
+
+# s = '공격반은 몬스터는 {}의 데미지를 받았습니다.'
+
+# print(s.format("inf"))
+# print(s.format("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"))
+# print(s.format("1"))
+
+#5번
+
+# age = 13
+# year = 2023 + 1 - 13
+# print('당신의 올해 나이는{}살 입니다. 당신은 {}년에 태어났습니다.'.format(age, year))
+
+#6번
+
+# s = 'hi'
+# print(s + ' ^^')
+
+# def smile(s):
+#     end = ':D'
+#     print(s + end)
+
+# smile("안녕하세요")
+# smile(" ")
+
+#7번
+
+# def expense(transportation):
+#     adult_expense = 0
+#     child_expense = 0
+
+#     if transporationt == "bus":
+#         adult_expense = 40000
+#         child_expense = 20000
+#     elif transporationt =="ship":
+#         adult_expense = 30000
+#         child_expense = 15000
+#     else:
+#         adult_expense = 70000
+#         child_expense = 35000
+
+#     print("{}의 성인 요금은 {}원, 어린이 요금은 {}원 입니다.".format(transportation, adult_expense, child_expense))
+
+
+# transporationt = input("사용할 교통편 : ")
+# expense(transporationt)
